b9cfdc4fb8ad701d3a5e20d623bd64d2de35e928
Elişa <macbook@Elisa.local>
2024-08-22 00:36:18 +0300

    lbx update

--- react-app/src/pages/homePage.js
+++ react-app/src/pages/homePage.js
@@ -6,7 +6,7 @@ import { ImgCardComponent } from '../components/card/imgCardComponent';
 import { BussinessSliderComponent } from "../components/home/bussinessSliderComponent";
 import { ContactFormComponent } from '../components/form/contactFormComponent';
 import { ImgComponent } from '../components/img/imgComponent';
-import {LightboxComponent} from "../components/common/lightboxComponent";
+import { InfoLbxComponent } from '../components/lightbox/infoLbxComponent';
 
 const HomePage = () => {
     const benefitArr = [
@@ -40,7 +40,8 @@ const HomePage = () => {
                 </div>
                 <ContactFormComponent title={"Join Our LMS"} text="We offer reliable Learning Management System that completely fulfills the requirements from authorities. User friendly interface, easy to read reports and built-in guidance will simplify your learning flow." />
             </div>
-            <LightboxComponent/>
+            <InfoLbxComponent icon={"done.svg"} title={"Your message has been sent"} text={"Your message has reached us. <br/> We will contact you as soon as possible."} btnText={"Done"} btnClass="green-btn"/>
+            <InfoLbxComponent icon={"warning.svg"} title={"Something Went Wrong"} text={"Lorem ipsum dolor sit amet consectetur. <br/> Quis eros orci quis amet pretium."} btnText={"Done"} btnClass="red-btn"/>
         </div>
     );
 }
