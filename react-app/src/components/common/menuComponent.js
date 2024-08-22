865298b62e5e4ed04ed0822e05b54cc939a6f865
afrkncgds <frkncagdas@gmail.com>
2024-08-22 11:42:10 +0300

    updated

--- react-app/src/components/common/menuComponent.js
+++ react-app/src/components/common/menuComponent.js
@@ -2,6 +2,7 @@ import { LOGIN } from "../../constants/routePaths";
 import { ButtonComponent } from "../button/buttonComponent"
 import {MobileMenuComponent} from "./mobileMenuComponent";
 import {useNavigate} from "react-router-dom";
+import {SvgImgComponent} from "../img/svgImgComponent";
 
 export const MenuComponent = ({ data, btnText, icon }) => {
     const navigate = useNavigate();
@@ -13,10 +14,16 @@ export const MenuComponent = ({ data, btnText, icon }) => {
         <div className="menu-list">
             {
                 data.map((item, key) => (
-                    <a href={item.link}  className="menu-item" key={key}>
+                    <a href={item.link} className="menu-item" key={key}>
                         {
                             <span>{item.name}</span>
+
                         }
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 8L10 13L5 8" stroke="#1F2937" strokeWidth="1.5" strokeLinecap="round"
+                                  strokeLinejoin="round"/>
+                        </svg>
+
 
                     </a>
                 ))
