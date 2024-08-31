7c5d87f59bac7f05a0d3377ba9b67657f280f688
Elişa <macbook@Elisa.local>
2024-09-01 01:04:11 +0300

    header update

--- react-app/src/components/dsLayout/dsHeader.js
+++ react-app/src/components/dsLayout/dsHeader.js
@@ -1,18 +1,21 @@
-import React, {   } from "react";
+import React, { } from "react";
 import { LogoComponent } from "../common/logoComponent";
-import {DsMenuComponent} from "../dsComponents/dsMenuComponent";
+import { DsMenuComponent } from "../dsComponents/dsMenuComponent";
+import { useLocation } from "react-router-dom";
+import { LOGIN } from "../../constants/routePaths";
 
-const DsHeader = ()=> {
-
-    return(
+const DsHeader = () => {
+    const loc = useLocation();
+    return (
         <header className="header ds">
             <div className="header--wrapper safe-area">
-                <LogoComponent icon={"logo.svg"}/>
-                <div class="menu-block">
-                    <DsMenuComponent/>
-
-                </div>
-
+                <LogoComponent icon={"logo.svg"} />
+                {
+                    loc.pathname !== LOGIN &&
+                    <div className="menu-block">
+                        <DsMenuComponent />
+                    </div>
+                }
             </div>
         </header>
     )
